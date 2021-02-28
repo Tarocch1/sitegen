c3c96f21b3312c335483d43feac43b34455cf45b
Tarocch1 <huiben.fang@gmail.com>
2021-02-28 13:22:48 +0800

    加入 build 命令

--- sitegen.config.example.js
+++ sitegen.config.example.js
@@ -4,14 +4,17 @@ module.exports = {
   title: '',
   description: '',
   publicFolder: 'public',
-  base: '',
-  watch: true,
   markdown: {
     lineNumbers: false,
     timeFormatter: time => dayjs(time).format('YYYY-MM-DD HH:mm:ss'),
   },
-  server: {
+  build: {
+    dist: 'dist',
+    base: '',
+  },
+  dev: {
     host: '0.0.0.0',
     port: 3000,
+    watch: true,
   },
 };
