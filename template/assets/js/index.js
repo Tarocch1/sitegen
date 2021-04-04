bbb88dfeaf20d0122d2b637a4faef4dd2b104fe6
Tarocch1 <huiben.fang@gmail.com>
2021-04-04 12:15:07 +0800

    feat: ✨ 支持 mermaid

---
+++ template/assets/js/index.js
@@ -0,0 +1,23 @@
+import mermaid from 'mermaid';
+
+mermaid.initialize({
+  startOnLoad: true,
+  flowchart: {
+    useMaxWidth: false,
+  },
+  sequence: {
+    useMaxWidth: false,
+  },
+  gantt: {
+    useMaxWidth: false,
+  },
+  journey: {
+    useMaxWidth: false,
+  },
+  er: {
+    useMaxWidth: false,
+  },
+  pie: {
+    useMaxWidth: false,
+  },
+});
