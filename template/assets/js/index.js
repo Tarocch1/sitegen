697279db797b55f218cb5e50e4fe60e68bc5f692
Tarocch1 <huiben.fang@gmail.com>
2021-04-16 21:34:51 +0800

    feat: ✨ 引入 Feather 图标集

--- template/assets/js/index.js
+++ template/assets/js/index.js
@@ -1,8 +1,10 @@
+import feather from 'feather-icons';
 import { initHash } from './hash';
 import { initAside } from './aside';
 import { initMermaid } from './mermaid';
 
 window.addEventListener('load', function () {
+  feather.replace();
   initHash();
   initAside();
   initMermaid();
